8897b22c55ea3c3ebb051f83afa565c3d0ecd010
Jordan <jordan.angeline1998@gmail.com>
2021-08-19 14:14:47 +0100

    add output queue

--- AcmeSubProject/HttpEndpoint/__init__.py
+++ AcmeSubProject/HttpEndpoint/__init__.py
@@ -1,16 +1,34 @@
 import logging
 import time
 import azure.functions as func
+import uuid
+import json
 
 
-def main(request: func.HttpRequest) -> func.HttpResponse:
+def main(request: func.HttpRequest, message: func.Out[str],queue: func.Out[str]) -> func.HttpResponse:
     logging.info('HTTP trigger function received a request.')
     start = time.time()
 
     req_body = request.get_json()
     subtitle = req_body.get("subtitle")
+    languages = req_body.get("languages")
 
-    time.sleep(5) # Simulating 5 seconds of cpu-intensive processing
+    rowKey = str(uuid.uuid4())
+
+    for language in languages:
+        event_data = {
+            "RowKey":rowKey,
+            "languageCode": language
+        }
+        queue.set(json.dumps(event_data))
+
+
+    data = {
+        "Subtitle": subtitle,
+        "PartitionKey": "message",
+        "RowKey": rowKey
+    }
+    message.set(json.dumps(data))
     end = time.time()
     processingTime = end - start
 
